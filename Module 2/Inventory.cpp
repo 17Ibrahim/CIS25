c32610bd1402b4905d0d902227d24e53086d77bd
Ibrahim <163785068+17Ibrahim@users.noreply.github.com>
2026-06-25 15:13:51 -0700

    Add files via upload

---
+++ Module 2/Inventory.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Hello Inventory System!\n" ;
+    cout<< "SIZE OF INT IS "<< sizeof(int) << " bytes \n";
+    cout<< "SIZE OF FLOAT IS "<< sizeof(float) << " bytes \n";
+    cout<< "SIZE OF CHAR IS "<< sizeof(char) << " bytes \n";
+    return 0;
+}
+
